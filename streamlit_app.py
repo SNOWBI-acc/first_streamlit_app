a0f9e9cc8dc63ec28a0e328483ed3d2e64051fd7
SNOWBI-acc <113721211+SNOWBI-acc@users.noreply.github.com>
2022-09-16 16:22:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 
 import streamlit
-streamlit.title('My Parents Now Healthy Diner')
-
 streamlit.header('Breakfast Menu')
-streamlit.text('Omego 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
